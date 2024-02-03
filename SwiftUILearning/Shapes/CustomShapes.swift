089047eccb6dd6481c88b28ab88545a587507d6a
Ajay <ajay.nellori@gmail.com>
2024-02-03 10:44:10 +0530

    Worked on custom shapes Added Pentagon and Capsule shapes

--- SwiftUILearning/Shapes/CustomShapes.swift
+++ SwiftUILearning/Shapes/CustomShapes.swift
@@ -38,3 +38,38 @@ struct Diamond: Shape {
         }
     }
 }
+
+struct PacMan: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.midY))
+            path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.height/2, startAngle: Angle(degrees: -20), endAngle: Angle(degrees: 20), clockwise: true)
+        }
+    }
+}
+
+struct Pentagon: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: .zero)
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        }
+    }
+}
+
+struct HalfCapsule: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: .zero)
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+            path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.height/2, startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 180), clockwise: false)
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        }
+    }
+}
--- SwiftUILearning/Shapes/CustomShapesDemo.swift
+++ SwiftUILearning/Shapes/CustomShapesDemo.swift
@@ -12,8 +12,12 @@ struct CustomShapesDemo: View {
         VStack(alignment: .center, spacing: 20) {
             Spacer()
             HStack {
-                PlainLine(height: 1, lineWidth: 1, color: Color.black)
-                DashedLine(height: 1, lineWidth: 1, lineLength: 5, color: Color.black)
+                SolidLine(color: Color.black)
+                DashedLine(
+                    lineLength: 5,
+                    color: Color.black
+                )
+                SolidDivider(height: 1)
             }
             HStack {
                 Image(.profileIcon)
@@ -23,11 +27,17 @@ struct CustomShapesDemo: View {
                 SolidTriangle(width: 100, height: 100, color: Color.brown)
                     .rotationEffect(.degrees(180))
                 DashedTriangle(width: 100, height: 100, dashLength: 5, lineWidth: 1, color: Color.brown)
-                
             }
             HStack {
                 SolidDiamond(width: 100, height: 100, color: .red)
                 DashedDiamond(width: 100, height: 100, lineWidth: 1, dashLength: 5, color: .red)
+                SolidPentagon(width: 100, height: 100, color: .red)
+            }
+            HStack {
+                PacMan()
+                    .frame(width: 100, height: 100)
+                
+                SolidHalfCapsule(width: 100, height: 100, color: Color.green)
             }
             Spacer()
         }
@@ -38,26 +48,33 @@ struct CustomShapesDemo: View {
 }
 
 // MARK: - LINE
-struct PlainLine: View {
-    let height: CGFloat
-    let lineWidth: CGFloat
+struct SolidLine: View {
     let color: Color
     var body: some View {
         Line()
-            .stroke(color, lineWidth: lineWidth)
+            .stroke(color, lineWidth: 1)
+            .frame(maxWidth: .infinity)
+            .frame(height: 1)
+    }
+}
+
+/// Use `SolidDivider` if you want to give desired height otherwise use `SolidLine` if you want to give height = 1
+struct SolidDivider: View {
+    let height: CGFloat
+    var body: some View {
+         Divider()
             .frame(height: height)
             .frame(maxWidth: .infinity)
+            .background(.green)
     }
 }
 struct DashedLine: View {
-    let height: CGFloat
-    let lineWidth: CGFloat
     let lineLength: CGFloat
     let color: Color
     var body: some View {
         Line()
-            .stroke(color, style: StrokeStyle(lineWidth: lineWidth, dash: [lineLength]))
-            .frame(height: height)
+            .stroke(color, style: StrokeStyle(lineWidth: 1, dash: [lineLength]))
+            .frame(height: 1)
             .frame(maxWidth: .infinity)
     }
 }
@@ -95,6 +112,7 @@ struct SolidDiamond: View {
         Diamond()
             .fill(color)
             .frame(width: width, height: height)
+            .padding(.horizontal, -(width * 0.2))
     }
 }
 struct DashedDiamond: View {
@@ -107,5 +125,32 @@ struct DashedDiamond: View {
         Diamond()
             .stroke(color, style: .init(lineWidth: lineWidth, dash: [dashLength]))
             .frame(width: width, height: height)
+            .padding(.horizontal, -(width * 0.2))
+    }
+}
+
+struct SolidPentagon: View {
+    let width: CGFloat
+    let height: CGFloat
+    let color: Color
+    var body: some View {
+        Pentagon()
+            .fill(color)
+            .frame(width: width, height: height)
+            .rotationEffect(Angle(degrees: 180))
+
+    }
+}
+
+struct SolidHalfCapsule: View {
+    let width: CGFloat
+    let height: CGFloat
+    let color: Color
+    var body: some View {
+        HalfCapsule()
+            .fill(color)
+            .frame(width: width, height: height)
+            .rotationEffect(Angle(degrees: 90))
+
     }
 }
